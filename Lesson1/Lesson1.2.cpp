23c0e108fb3e3f8c92f0d72e4c1b96ef035cc908
DaireÓM <thedaire@gmail.com>
2017-05-22 15:20:37 +0100

    First commit
    First commit to github for C++ Day Course

---
+++ Lesson1/Lesson1.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int aNumber;
+  cout << "Pick a number?\n";
+  cin >> aNumber;
+  cout << "Is your number " << aNumber << "?";
+}
---
+++ Lesson1/Lesson1.3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+}
+
+
+void pickNumber() {
+  int aNumber;
+  cout << "Pick a number?\n";
+  cin >> aNumber;
+  cout << "Is your number " << aNumber << "?";
+}
+
+int pickNumber
