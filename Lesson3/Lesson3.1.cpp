c6ec3b0610e3a30bf0bd06b77acbcc66315cb998
DaireÓM <thedaire@gmail.com>
2017-05-23 12:23:32 +0100

    Lesson 3 updated

--- Lesson3/Lesson3.1.cpp
+++ Lesson3/Lesson3.1.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
 
 using namespace std;
+
+
+int main() {
+  int aNumber = 10;
+  int *aPointerToANumber = &aNumber;
+  *aPointerToANumber = 12;
+  aNumber = *aPointerToANumber;
+
+
+  cout << aNumber << endl;
+  cout << "Memory address of number " << aPointerToANumber << endl;
+  cout << "Value of number " << *aPointerToANumber << endl;
+
+  cout << "Enter Number" << endl;
+  cin >> aNumber;
+
+
+  cout << aNumber << endl;
+  cout << "Memory address of number " << aPointerToANumber << endl;
+  cout << "Value of number " << *aPointerToANumber << endl;
+
+  cout << "HI";
+
+
+
+}
